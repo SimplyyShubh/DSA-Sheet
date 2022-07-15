2af58a8581c720b027d58e00e249209f7756f5cb
Simply Shubh <simplyy11@gmail.com>
2022-07-16 00:39:18 +0530

    Unrunned Solution

--- Choclate Distribution Problem.cpp
+++ Choclate Distribution Problem.cpp
@@ -1,8 +1,25 @@
 // https://www.geeksforgeeks.org/chocolate-distribution-problem/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int solution(int packets[], int m)
+{
+    // int n = sizeof(packets) / sizeof(packets[0]); // To get the size of the array packets
+    int n = 4 ;
+    sort(packets, packets + n);                   // Sorting the array till its length
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << packets[i] << ", ";
+    }
 
     return 1 ;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    solution(arr, 1);
+    return 1;
 }
