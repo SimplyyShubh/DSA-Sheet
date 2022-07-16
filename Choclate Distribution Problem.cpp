0ddbb2fe21d0e5162262a17406fc6d60e460cd89
Simply Shubh <simplyy11@gmail.com>
2022-07-17 01:09:47 +0530

    Done ChoclateDistribution

--- Choclate Distribution Problem.cpp
+++ Choclate Distribution Problem.cpp
@@ -3,23 +3,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solution(int packets[], int m)
+int solution(int arr[], int n, int m)
 {
-    // int n = sizeof(packets) / sizeof(packets[0]); // To get the size of the array packets
-    int n = 4 ;
-    sort(packets, packets + n);                   // Sorting the array till its length
+  
+    sort(arr, arr + n) ;
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << packets[i] << ", ";
+    int min = INT_MAX ;
+
+    for(int i = 0 ; i < n ; i++){
+        cout << arr[i] << ", ";
     }
-
-    return 1 ;
+        cout << '\n' ;
+    for (int i = 0 , j = i+m -1 ; j < n; i++ , j++)
+    {
+        if (min > (arr[j] - arr[i]))
+            min = arr[j] - arr[i] ;
+    
+    }
+    return min ;
 }
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4};
-    solution(arr, 1);
-    return 1;
+    int packets[] = {12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 
+                     28, 42, 30, 44, 48, 43, 50};
+    int n = sizeof(packets) / sizeof(packets[0]);
+
+    cout << solution(packets, n, 7) ;
+
+        return 1;
 }
